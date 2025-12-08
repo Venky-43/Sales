0781ed2459c208de1fae80e05be1b734637cfbaf
Venkannababu Kothapalli <161434793+Venky-43@users.noreply.github.com>
2025-12-08 11:05:04 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,14 +1,12 @@
 import axios from "axios";
 
-// Create axios instance
 const API = axios.create({
-  baseURL: "https://sales-2-6b8q.onrender.com/api/sales",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
-
-// Function to fetch sales with query params
 export const fetchSales = async (params) => {
   const res = await API.get("/", { params });
   return res.data;
 };
+
 
